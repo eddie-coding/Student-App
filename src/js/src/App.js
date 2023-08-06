eac7fe9f28225c8723773a8fa63673193aa56f60
eddie-coding <adityabahuguna007@gmail.com>
2023-08-06 20:15:54 +0530

    Imported antd UI library for styling

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,7 +1,11 @@
 //import logo from './logo.svg';
 import { Component } from 'react';
+import Container from './Container';
 import './App.css';
 import { getAllStudents } from './client';
+import {
+  Table
+} from 'antd';
 
 class App extends Component{
 
@@ -30,17 +34,54 @@ class App extends Component{
 
     if (students && students.length) {
 
-      return students.map((student,index) => {
-        return (
-          <div key={index}>
-            <h2>{student.studentId}</h2>
-            <p>{student.firstName}</p>
-            <p>{student.lastName}</p>
-            <p>{student.gender}</p>
-            <p>{student.email}</p>
-          </div>
-        );
-      })
+      const columns = [
+        {
+          title: 'Student ID',
+          dataIndex: 'studentId',
+          key: 'studentId'
+        },
+        {
+          title: 'First Name',
+          dataIndex: 'firstName',
+          key: 'firstName'
+        },
+        {
+          title: 'Last Name',
+          dataIndex: 'lastName',
+          key: 'lastName'
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email'
+        },
+        {
+          title: 'Gender',
+          dataIndex: 'gender',
+          key: 'gender'
+        }
+      ];
+
+      return (
+        <Container>
+          <Table 
+            dataSource={students} 
+            columns={columns} 
+            rowKey='studentId'/>
+        </Container>
+      );
+
+      // return students.map((student,index) => {
+      //   return (
+      //     <div key={index}>
+      //       <h2>{student.studentId}</h2>
+      //       <p>{student.firstName}</p>
+      //       <p>{student.lastName}</p>
+      //       <p>{student.gender}</p>
+      //       <p>{student.email}</p>
+      //     </div>
+      //   );
+      // })
     }
 
     return <h1>No Students found</h1>
