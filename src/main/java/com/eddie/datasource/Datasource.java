2061a6f178e95ec86fe84f545fba2ed5aa72c39e
eddie-coding <adityabahuguna007@gmail.com>
2023-08-12 12:36:07 +0530

    connecting docker psql database to the backend application

---
+++ src/main/java/com/eddie/datasource/Datasource.java
@@ -0,0 +1,22 @@
+package com.eddie.datasource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+@Configuration
+public class Datasource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }    
+}
