b6b647cf8d322b3e522b41fc8c84911abff2e65a
eddie-coding <adityabahuguna007@gmail.com>
2023-08-13 16:09:30 +0530

    Added AddStudent form

--- src/js/src/forms/AddStudentForm.js
+++ src/js/src/forms/AddStudentForm.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 import { Formik } from 'formik';
-import { Input, Button } from 'antd'
+import { Input, Button, Tag } from 'antd'
 
 const inputBottomMargin = {marginBottom: '5px'};
+const tagStyle = {backgroundColor: '#f50', color:'white', ...inputBottomMargin};
 
 class AddStudentForm extends Component {
     render () {
         return (
                 <Formik
-                initialValues={{ firstName: '', password: '' }}
+                initialValues={{ firstName: '', lastName: '', email:'', gender:'' }}
                 validate={values => {
                     const errors = {};
-                    if (!values.firstName) {
-                    errors.firstName = 'Required';
+
+                    if(!values.firstName) {
+                        errors.firstName = 'First Name Required';
+                    }
+
+                    if(!values.lastName) {
+                        errors.lastName = 'Last Name Required';
+                    }
+                    
+                    if (!values.email) {
+                    errors.email = 'Email Required';
                     } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.firstName)
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                     ) {
-                    errors.firstName = 'Invalid email address';
+                        errors.email = 'Invalid email address';
+                    }
+
+                    if(!values.gender) {
+                        errors.gender = 'Gender Required';
+                    } else if (!['MALE', 'male', 'FEMALE', 'female'].includes(values.gender)) {
+                        errors.gender = 'Gender must be (MALE, male, FEMALE, female)';
                     }
                     return errors;
-                    }}
+                }}
                 onSubmit={(values, { setSubmitting }) => {
                     setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
@@ -46,7 +62,7 @@ class AddStudentForm extends Component {
                         value={values.firstName}
                         placeholder='First Name. E.g Adiya'
                     />
-                    {errors.firstName && touched.firstName && errors.firstName}
+                    {errors.firstName && touched.firstName && <Tag style={tagStyle}>{errors.firstName}</Tag>}
                     <Input
                         style={inputBottomMargin}
                         name="lastName"
@@ -55,7 +71,7 @@ class AddStudentForm extends Component {
                         value={values.lastName}
                         placeholder='Last Name. E.g Bahuguna'
                     />
-                    {errors.lastName && touched.lastName && errors.lastName}
+                    {errors.lastName && touched.lastName && <Tag style={tagStyle}>{errors.lastName}</Tag>}
                     <Input
                         style={inputBottomMargin}
                         name="email"
@@ -65,7 +81,7 @@ class AddStudentForm extends Component {
                         value={values.email}
                         placeholder='email. E.g @gmail.com'
                     />
-                    {errors.email && touched.email && errors.email}
+                    {errors.email && touched.email && <Tag style={tagStyle}>{errors.email}</Tag>}
                     <Input
                         style={inputBottomMargin}
                         name="gender"
@@ -74,8 +90,8 @@ class AddStudentForm extends Component {
                         value={values.gender}
                         placeholder='Gender. E.g Male or Female'
                     />
-                    {errors.gender && touched.gender && errors.gender}
-                    <Button type="submit" disabled={isSubmitting}>
+                    {errors.gender && touched.gender && <Tag style={tagStyle}>{errors.gender}</Tag>}
+                    <Button style={tagStyle} type="submit" disabled={isSubmitting}>
                         Submit
                     </Button>
                     </form>
